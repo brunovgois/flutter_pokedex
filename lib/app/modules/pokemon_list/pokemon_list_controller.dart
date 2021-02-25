d741a015b1f2c22081fa497bb815cb947e8a2b26
Bruno Gois <brunovgois@gmail.com>
2021-02-25 14:59:44 -0300

    fix bug on parse of Json Object

--- lib/app/modules/pokemon_list/pokemon_list_controller.dart
+++ lib/app/modules/pokemon_list/pokemon_list_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter_pokedex/app/entities/pokemon.dart';
 import 'package:mobx/mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -18,33 +20,13 @@ abstract class _PokemonListControllerBase with Store {
     var result = await Dio().get(
         "http://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json");
 
-    print(result.data.toString());
-    // parsePokemons(result.data);
+    // print(result.data.toString());
+    parsePokemons(result.data);
   }
 
   void parsePokemons(responseData) {
-    // List<Pokemon> x =
-    //     responseData["pokemon"].map((e) => Pokemon.fromJson(e)).toList();
-
-    print(Pokemon.fromJson(responseData));
-
-    // var decode = json.decode(responseData)["pokemon"].cast<Pokemons>();
-
-    // var map = LinkedHashMap<String, dynamic>.from(responseData);
-
-    // Pokemons.fromJson(responseData); //Unhandled Exception: type 'String' is not a subtype of type 'Map<String, dynamic>'
-    // print(responseData);
-
-    // var x = (responseData as List).map((e) => Pokemon.fromJson(e)).toList();
-    // print(Map<String, dynamic>.from(responseData));
-    // var decoded = json.decode(responseData);
-    // List<Pokemon> temp = [];
-    // for (var item in decoded) {
-    //   temp.add(Pokemon.fromJson(item));
-    // }
-
-    // final List<Pokemon> parsedPokeList = json.decode(responseBody);
-    // List<Pokemon> list =
-    //     parsedPokeList.map((e) => parsedPokeList.fromJson(e).toList());
+    List response = json.decode(responseData)["pokemon"];
+    List<Pokemon> x = response.map((e) => Pokemon.fromJson(e)).toList();
+    print(x.length);
   }
 }
