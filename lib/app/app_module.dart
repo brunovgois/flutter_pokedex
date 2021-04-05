9f79e36e4dc173823faaee421ce62fbff94fc17e
brunovgois <brunovgois@gmail.com>
2021-04-05 00:10:33 -0300

    remove unused bind

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,15 +1,12 @@
 import 'package:flutter_pokedex/app/modules/pokemon_main/pokemon_main_module.dart';
 
-import 'app_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_pokedex/app/app_widget.dart';
 
 class AppModule extends MainModule {
   @override
-  List<Bind> get binds => [
-        Bind((i) => AppController()),
-      ];
+  List<Bind> get binds => [];
 
   @override
   List<ModularRouter> get routers => [
